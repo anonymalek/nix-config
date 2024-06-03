33a42cfc285fb89ac78a36ae51a82ddda6b04b1d
alek <alekthesixth@gmail.com>
2024-06-03 20:40:57 +0000

    updated nvidia.nix

--- system/profiles/core/nvidia.nix
+++ system/profiles/core/nvidia.nix
@@ -40,6 +40,5 @@
 	# Optionally, you may need to select the appropriate driver version for your specific GPU.
 	package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-	
 	allowedUnfree = [ "nvidia-x11" "nvidia-settings" ];
 }
