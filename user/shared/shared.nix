ee3f06af6207af29540bae2b140ee64c0a87b030
Niki <>
2024-06-09 21:45:45 +0000

    user [shared]: added required pinentry-program config in gpg-agent.conf

--- user/shared/shared.nix
+++ user/shared/shared.nix
@@ -6,4 +6,6 @@
 		(writeShellScriptBin "unir" ''NIXPKGS_ALLOW_UNFREE=1 nir'')
 		(writeShellScriptBin "nir" ''nix run nixpkgs#$1 -- ''${@:2}'')
 	];
+
+	home.file.".gnupg/gpg-agent.conf".text = "pinentry-program /run/current-system/sw/bin/pinentry";
 }
