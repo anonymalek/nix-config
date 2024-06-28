a164f1aab253a56d74233fbd19720548869fc860
Niki <>
2024-06-28 18:55:00 +0000

    system [slock]: cleaned up derivation.nix system [dmenu]: cleaned up derivation.nix

--- system/profiles/core/sources/dmenu/derivation.nix
+++ system/profiles/core/sources/dmenu/derivation.nix
@@ -1,13 +1,21 @@
-{ stdenv, pkgs }:
-with pkgs;
-
-stdenv.mkDerivation rec {
+{ stdenv, pkgs }: stdenv.mkDerivation {
 	name = "dmenu";
 
 	src = ./.;
 
-	nativeBuildInputs = [ gnumake glibc glibc.static xorg.libX11 xorg.libXinerama xorg.libXft fontconfig ];
-	buildInputs = [ glibc.static ];
+	nativeBuildInputs = with pkgs; [
+		gnumake
+		glibc
+		glibc.static
+		xorg.libX11
+		xorg.libXinerama
+		xorg.libXft
+		fontconfig
+	];
+
+	buildInputs = with pkgs; [
+		glibc.static
+	];
 
 	buildPhase = "make";
 	installPhase = "DESTDIR=$out make install";
--- system/profiles/core/sources/slock/derivation.nix
+++ system/profiles/core/sources/slock/derivation.nix
@@ -1,13 +1,24 @@
-{ stdenv, pkgs }:
-with pkgs;
-
-stdenv.mkDerivation rec {
+{ stdenv, pkgs }: stdenv.mkDerivation  {
 	name = "slock";
 
 	src = ./.;
 
-	nativeBuildInputs = [ gnumake glibc glibc.static xorg.libX11 xorg.libXrandr xorg.libXext xorg.libXinerama xorg.libXft libxcrypt fontconfig ];
-	buildInputs = [ glibc.static ];
+	nativeBuildInputs = with pkgs; [
+		gnumake
+		glibc
+		glibc.static
+		xorg.libX11
+		xorg.libXrandr
+		xorg.libXext
+		xorg.libXinerama
+		xorg.libXft
+		libxcrypt
+		fontconfig
+	];
+
+	buildInputs = with pkgs; [
+		glibc.static
+	];
 
 	buildPhase = "make";
 	installPhase = "DESTDIR=$out make install";
