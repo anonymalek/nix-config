a99566ce8100dadaa33799325cb188bd28a3106a
Niki <>
2024-07-13 20:31:24 +0000

    user [niki]: removed -i from ln calls on the non nix helper link.sh

--- user/niki/config/link.sh
+++ user/niki/config/link.sh
@@ -2,21 +2,21 @@
 
 BASEDIR=$(dirname $(realpath "$0"))
 
-ln -sin -T $BASEDIR/qutebrowser ~/.config/qutebrowser
-ln -sin -T $BASEDIR/kitty ~/.config/kitty
-ln -sin -T $BASEDIR/awesome ~/.config/awesome
-ln -sin -T $BASEDIR/sxhkd ~/.config/sxhkd
-ln -sin -T $BASEDIR/fonts ~/.local/share/fonts
-ln -sin -T $BASEDIR/tmux ~/.config/tmux
-ln -sin -T $BASEDIR/gitconfig ~/.gitconfig
+ln -sn -T $BASEDIR/qutebrowser ~/.config/qutebrowser
+ln -sn -T $BASEDIR/kitty ~/.config/kitty
+ln -sn -T $BASEDIR/awesome ~/.config/awesome
+ln -sn -T $BASEDIR/sxhkd ~/.config/sxhkd
+ln -sn -T $BASEDIR/fonts ~/.local/share/fonts
+ln -sn -T $BASEDIR/tmux ~/.config/tmux
+ln -sn -T $BASEDIR/gitconfig ~/.gitconfig
 
-ln -sin -T $BASEDIR/emacs/emacs.el ~/.emacs
-ln -sin -T $BASEDIR/emacs/emacs.d ~/.emacs.d
+ln -sn -T $BASEDIR/emacs/emacs.el ~/.emacs
+ln -sn -T $BASEDIR/emacs/emacs.d ~/.emacs.d
 
-ln -sin -T $BASEDIR/vim/vimrc ~/.vimrc
-ln -sin -T $BASEDIR/vim ~/.config/nvim
+ln -sn -T $BASEDIR/vim/vimrc ~/.vimrc
+ln -sn -T $BASEDIR/vim ~/.config/nvim
 
-ln -sin -T $BASEDIR/mpd ~/.config/mpd
-ln -sin -T $BASEDIR/ncmpcpp ~/.config/ncmpcpp
+ln -sn -T $BASEDIR/mpd ~/.config/mpd
+ln -sn -T $BASEDIR/ncmpcpp ~/.config/ncmpcpp
 
-ln -sin -T $BASEDIR/bashrc ~/.bashrc
+ln -sn -T $BASEDIR/bashrc ~/.bashrc
