f91f4c303a91f4e58d919542aafaba4f5b8b3fba
Niki <>
2024-07-15 21:45:50 +0000

    user [niki/emacs]: now using use-package whenever possible

--- user/niki/config/emacs/emacs.el
+++ user/niki/config/emacs/emacs.el
@@ -1,69 +1,25 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(custom-set-variables
- '(package-selected-packages
-   '(auctex multiple-cursors undo-tree evil-collection pdf-tools popup-imenu popwin company eglot magit nix-mode projectile ivy lua-mode org-bullets)))
-
-(package-initialize)
-
-(defun become-evil ()
-  (interactive)
-  (setq evil-want-keybinding nil)
-  (setq evil-undo-system 'undo-tree)
-  (evil-mode 1)
-  (evil-collection-init)
-  (modify-syntax-entry ?_ "w")
-  )
-
-;; (become-evil)
-
-(keymap-global-set "C-c c" '(lambda () (interactive) (find-file "~/.emacs")))
-(keymap-global-set "C-S-v" '(lambda () (interactive) (set-rectangular-region-anchor)))
+(load-theme 'modus-vivendi)
 
 (ido-mode 1)
-
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 
 (global-display-line-numbers-mode 1)
 
-(setq-default display-line-numbers-type 'relative
-			  scroll-step 1
-			  scroll-margin 10
-			  use-dialog-box nil
-			  tab-width 4
-			  make-backup-files nil
-			  )
+(setq-default
+ display-line-numbers-type 'relative
+ scroll-step 1
+ scroll-margin 10
+ use-dialog-box nil
+ tab-width 4
+ make-backup-files nil
+ )
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(add-hook
- 'org-mode-hook
- (lambda ()
-   (org-bullets-mode 1)
-   (auto-fill-mode 1)
-   ))
-
-(with-eval-after-load "org"
-  (define-key org-mode-map (kbd "C-M-n") 'org-metadown)
-  (define-key org-mode-map (kbd "C-M-p") 'org-metaup)
-  (define-key org-mode-map (kbd "M-p") 'org-backward-heading-same-level)
-  (define-key org-mode-map (kbd "M-n") 'org-forward-heading-same-level)
-
-  (define-key org-mode-map (kbd "C-M-b") 'org-promote-subtree)
-  (define-key org-mode-map (kbd "C-M-f") 'org-demote-subtree)
-
-  (define-key org-mode-map (kbd "C-M-a") 'org-shiftleft)
-  (define-key org-mode-map (kbd "C-M-e") 'org-shiftright)
-
-  (define-key org-mode-map (kbd "C-M-k") 'org-shiftup)
-  (define-key org-mode-map (kbd "C-M-j") 'org-shiftdown)
-
-  (define-key org-mode-map (kbd "M-<return>") 'org-insert-subheading)
-  (define-key org-mode-map (kbd "C-<return>") 'org-insert-heading)
-  )
+(keymap-global-set "C-c c" '(lambda () (interactive) (find-file "~/.emacs")))
+(keymap-global-set "C-S-v" '(lambda () (interactive) (set-rectangular-region-anchor)))
 
 (setq c-default-style "linux"
 	  c-basic-offset 4)
@@ -72,36 +28,128 @@
   (interactive)
   (insert (shell-command-to-string "printf %s \"$(date '+%Y-%m-%d %H:%M:%S')\"")))
 
-(setq undo-tree-auto-save-history nil)
-(global-undo-tree-mode)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(setq mc/always-run-for-all t)
+(setq
+ package-selected-packages
+ '(
+   auctex
+   multiple-cursors
+   undo-tree
+   evil-collection
+   pdf-tools
+   popup-imenu
+   popwin
+   company
+   eglot
+   magit
+   nix-mode
+   projectile
+   ivy
+   lua-mode
+   org-bullets
+   ))
 
-(keymap-global-set "C-c d" 'eglot-find-declaration)
+(package-initialize)
 
-(keymap-global-set "C-c j" 'magit)
+(setq use-package-always-ensure t)
 
-(projectile-mode 1)
-(keymap-global-set "C-c p" 'projectile-command-map)
-(setq projectile-completion-system 'ivy)
+(use-package org
+  :config
+  (add-hook
+   'org-mode-hook
+   (lambda ()
+	 (org-bullets-mode 1)
+	 (auto-fill-mode 1)
+	 ))
+  :bind (
+		 :map org-mode-map
+		 ("C-M-n" . 'org-metadown)
+		 ("C-M-p" . 'org-metaup)
+		 ("M-p" . 'org-backward-heading-same-level)
+		 ("M-n" . 'org-forward-heading-same-level)
 
-(define-key projectile-mode-map (kbd "C-c f") 'projectile-find-file)
-(define-key projectile-mode-map (kbd "C-c g") 'projectile-grep)
+		 ("C-M-b" . 'org-promote-subtree)
+		 ("C-M-f" . 'org-demote-subtree)
 
-(popwin-mode 1)
+		 ("C-M-a" . 'org-shiftleft)
+		 ("C-M-e" . 'org-shiftright)
 
-(keymap-global-set "M-g i" 'popup-imenu)
+		 ("C-M-k" . 'org-shiftup)
+		 ("C-M-j" . 'org-shiftdown)
+
+		 ("M-<return>" . 'org-insert-subheading)
+		 ("C-<return>" . 'org-insert-heading)
+		 )
+  )
+
+(setq evil-want-keybinding nil) ;; required by evil-collection
+
+(use-package evil
+  :init
+  (setq evil-undo-system 'undo-tree)
+  :config
+  (evil-mode 1)
+
+  :bind (:map evil-normal-state-map
+			  ("C-u" . 'evil-scroll-up)))
+
+(use-package evil-collection
+  :config
+  (evil-collection-init)
+  )
+
+(use-package undo-tree
+  :config
+  (setq undo-tree-auto-save-history nil)
+  (global-undo-tree-mode))
+
+(use-package multiple-cursors
+  :config
+  (setq mc/always-run-for-all t))
+
+(use-package eglot
+  :bind
+  (("C-c d" . 'eglot-find-declaration)))
+
+(use-package magit
+  :bind
+  (("C-c j" . 'magit)))
+
+(use-package projectile
+  :bind
+  (("C-c p" . 'projectile-command-map)
+   :map projectile-mode-map
+   ("C-c f" . 'projectile-find-file)
+   ("C-c g" . 'projectile-grep))
+  :config
+  (setq projectile-completion-system 'ivy)
+  (projectile-mode 1))
+
+(use-package popwin
+  :config
+  (popwin-mode 1))
+
+(use-package popup-imenu
+  :bind
+  (("M-g i" . 'popup-imenu)))
 
 (use-package ivy
   :config
-  '((ivy-mode 1)
-	(ivy--regex-fuzzy 1)))
+  (ivy-mode 1))
 
-(add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
+(use-package nix-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode)))
 
-(require 'pdf-tools)
-(add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode))
-(add-hook 'pdf-view-mode-hook '(lambda () (interactive) (display-line-numbers-mode -1)))
+(use-package pdf-tools
+  :config
+  (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode))
+  (add-hook 'pdf-view-mode-hook '(lambda () (interactive) (display-line-numbers-mode -1))))
 
-;; (add-hook 'after-init-hook 'global-company-mode)
+(use-package company
+  :config
+  (global-company-mode)
+  )
 
