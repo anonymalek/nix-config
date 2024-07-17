e62a1b2d6c1f94e463b4f14e0a722b3b42292238
Niki <>
2024-07-17 17:01:41 +0000

    user [niki/emacs]: ???

--- user/niki/config/emacs/init.el
+++ user/niki/config/emacs/init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-vivendi))
  '(package-selected-packages
-   '(envrc auctex multiple-cursors undo-tree evil-collection pdf-tools popup-imenu company eglot magit nix-mode projectile ivy lua-mode org-bullets)))
+   '(magit evil envrc auctex multiple-cursors undo-tree evil-collection pdf-tools popup-imenu company eglot magit nix-mode projectile ivy lua-mode org-bullets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
