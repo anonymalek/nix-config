31b4a341df87005b58fca0bf402b788c72697b04
Lavínia Rodrigues <xenia@kernel32.xyz>
2024-08-10 17:46:36 -0300

    feat: add jade/malwareskunk user

---
+++ user/malwareskunk/dots/config/nvim/lua/configs/null-ls.lua
@@ -0,0 +1,29 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require("null-ls")
+
+local opts = {
+  sources = {
+    null_ls.builtins.formatting.clang_format,
+    null_ls.builtins.diagnostics.phpstan,
+    null_ls.builtins.formatting.phpcbf,
+    null_ls.builtins.formatting.phpcsfixer
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr
+      })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end
+      })
+    end
+  end
+}
+
+return opts
